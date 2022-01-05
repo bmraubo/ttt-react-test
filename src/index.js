dc02a79d4fd48ef498c71c6bfad4736bab1ed8f7
bmraubo <bmraubo@gmail.com>
2022-01-05 15:55:56 +0000

    jumpTo method added

--- src/index.js
+++ src/index.js
@@ -72,6 +72,13 @@ function Square(props) {
         });
     }
 
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        });
+    }
+
     render() {
         const history = this.state.history;
         const current = history[history.length - 1];
