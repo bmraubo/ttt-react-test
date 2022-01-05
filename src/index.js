3a51abc9961dd170f21718ed03a1a81590a76a7a
bmraubo <bmraubo@gmail.com>
2022-01-05 13:57:52 +0000

    Game class renders game status

--- src/index.js
+++ src/index.js
@@ -76,18 +76,32 @@ function Square(props) {
     }
 
     render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-    }
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = calculateWinner(current.squares);
+        let status;
+
+        if (winner) {
+            status = "Winner: " + winner;
+        } else {
+            status = "Next Player: " + (this.state.xIsNext ? "X" : "O");
+        }
+
+        return (
+            <div className="game">
+            <div className="game-board">
+                <Board 
+                    squares={current.squares}
+                    onClick={(i) => this.handleClick(i)}
+                />
+            </div>
+            <div className="game-info">
+                <div>{/* status */}</div>
+                <ol>{/* TODO */}</ol>
+            </div>
+            </div>
+        );
+        }
   }
 
   function calculateWinner(squares) {
